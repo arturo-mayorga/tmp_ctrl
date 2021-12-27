fb15164cc1ef3a30949aabc0c8b55441fc10c9e7
Arturo Mayorga <arturo@mayorga.io>
2021-12-27 02:52:06 -0500

    Temp Sensor Implementation

--- src/app-state.h
+++ src/app-state.h
@@ -1,16 +1,26 @@
 #ifndef app_state_h
 #define app_state_h
 
-struct AppState {
+struct AppState
+{
     float currentTemp;
     float targetTemp;
-    unsigned long  targetTempUpdateTime;
+    unsigned long targetTempUpdateTime;
     int buttonState;
     int knobState;
     int connectionStatus;
     float knobPercentage;
+
+    AppState()
+    {
+        currentTemp = 0;
+        targetTemp = 0;
+        targetTempUpdateTime = 0;
+        buttonState = 0;
+        knobState = 0;
+        connectionStatus = 0;
+        knobPercentage = 0;
+    }
 };
-
-
 
 #endif
--- src/temp-sensor-system.h
+++ src/temp-sensor-system.h
@@ -3,17 +3,19 @@
 
 #include "app-system.h"
 
-#include "i-ssd-wrapper.h"
+#include "i-dallas-temperature-wrapper.h"
 
 class TempSensorSystem : public AppSystem
 {
 private:
-    ISsdWrapper *_issd;
+    IDallasTemperatureWrapper *_dallasTempW;
+    unsigned long _lastUpdateTime;
 
 public:
-    TempSensorSystem(AppState *state, ISsdWrapper *issd) : AppSystem(state)
+    TempSensorSystem(AppState *state, IDallasTemperatureWrapper *dallasTempW) : AppSystem(state)
     {
-        _issd = issd;
+        _dallasTempW = dallasTempW;
+        _lastUpdateTime = 0;
     }
     virtual void setup();
     virtual void update(unsigned long time); // Time in milliseconds
